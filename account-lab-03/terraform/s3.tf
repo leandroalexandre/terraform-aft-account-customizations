7b29454e11206db36d0a64e08b9a38730b4ad69e
leandroalexandre <leandro.alexandre@gmail.com>
2022-07-08 14:00:14 -0300

    create account  lab-03

---
+++ account-lab-03/terraform/s3.tf
@@ -0,0 +1,6 @@
+data "aws_caller_identity" "current" {}
+
+resource "aws_s3_bucket" "account-02_bucket" {
+  bucket = "aft-account-03-${data.aws_caller_identity.current.account_id}"
+  acl    = "private"
+}
