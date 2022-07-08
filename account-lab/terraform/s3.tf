f12acc9ec65cd368613688effe737e4e11669b4a
leandroalexandre <leandro.alexandre@gmail.com>
2022-07-08 11:55:28 -0300

    remove folder

--- account-lab/terraform/s3.tf
+++
@@ -1,6 +0,0 @@
-data "aws_caller_identity" "current" {}
-
-resource "aws_s3_bucket" "account-lab_bucket" {
-  bucket = "aft-account-lab-${data.aws_caller_identity.current.account_id}"
-  acl    = "private"
-}
